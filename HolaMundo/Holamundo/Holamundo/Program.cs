79122efa678724b504632d1ee5954abf6af0a115
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-03-15 15:58:29 -0300

    clase 1.1

--- HolaMundo/Holamundo/Holamundo/Program.cs
+++ HolaMundo/Holamundo/Holamundo/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Holamundo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Ingrese nombre: ");
+            string nombre = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Su nombre es {0}", nombre);    
+            Console.ReadKey();
         }
     }
 }
