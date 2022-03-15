90e67d99cbd16606726e7bbc415499d6c1e874e9
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-03-15 16:02:23 -0300

    uso de trim

--- HolaMundo/Holamundo/Holamundo/Program.cs
+++ HolaMundo/Holamundo/Holamundo/Program.cs
@@ -10,13 +10,14 @@ namespace Holamundo
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Clear();
+
             Console.WriteLine("Hola Mundo");
             Console.WriteLine("Ingrese nombre: ");
             string nombre = Console.ReadLine();
-            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ingrese Edad: ");
+            string edadtx = Console.ReadLine().Trim();
+            //TrimStart(); Quita espacios al inicio
+            //TrimEnd(); Quita espacios al final
             Console.WriteLine("Su nombre es {0}", nombre);    
             Console.ReadKey();
         }
