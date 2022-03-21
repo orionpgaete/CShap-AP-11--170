121c372bbdcacf61f9bbbf79c691bda8f31f1e50
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-03-21 17:15:25 -0300

    menu, Biblio Clase, uso de Partial

--- AdminPersonas/AdminPersonas/Program.cs
+++ AdminPersonas/AdminPersonas/Program.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace AdminPersonas
 {
-    class Program
+    public partial class Program
     {
         static void Main (string[] args)
         {
-
+            while (Menu()) ;
         }
 
         static bool Menu()
@@ -28,9 +28,9 @@ namespace AdminPersonas
             {
                 case "1": IngresarPersona();
                     break;
-                case "2": //mostrar
+                case "2": MostrarPersona();
                     break;
-                case "3": //Buscar
+                case "3": BuscarPersona();
                     break;
                 case "0": continuar = false;
                     break;
@@ -41,56 +41,6 @@ namespace AdminPersonas
         }
 
         
-        static void IngresarPersona()
-        {
-            string nombre;
-            uint telefono;
-            double peso;
-            double estatura;
-
-            Console.WriteLine("Bievenido al programa ultramente BKN");
-
-            bool esValido;
-
-            do
-            {
-                Console.WriteLine("Ingrese telefono");
-                string tel = Console.ReadLine().Trim();
-                esValido = UInt32.TryParse(tel, out telefono);
-            } while (!esValido);
-
-            do
-            {
-                Console.WriteLine("Ingrese peso");
-                string pes = Console.ReadLine().Trim();
-                esValido = Double.TryParse(pes, out peso);
-            } while (!esValido);
-
-            do
-            {
-                Console.WriteLine("Ingrese estatura");
-                string est = Console.ReadLine().Trim();
-                esValido = Double.TryParse(est, out estatura);
-            } while (!esValido);
-
-            do
-            {
-                Console.WriteLine("Ingrese nombre");
-                nombre = Console.ReadLine().Trim();
-            } while (nombre == string.Empty);
-
-            Persona p = new Persona();
-            p.Nombre = nombre;
-
-            Console.WriteLine("Nombre: {0}", nombre);
-            Console.WriteLine("Telefono : {0}", telefono);
-            Console.WriteLine("Peso : {0}", peso);
-            Console.WriteLine("Estatura : {0}", estatura);
-            Console.WriteLine("IMC es : {0}", peso / (estatura * estatura));
-            Console.ReadKey();
-        }
-
-                
-        }
+       
     }
 }
