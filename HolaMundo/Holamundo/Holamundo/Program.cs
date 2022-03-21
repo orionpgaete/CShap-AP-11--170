c3bbd766269f724113afde63b1dc87275f7d336a
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-03-21 15:18:54 -0300

    uso try catch

--- HolaMundo/Holamundo/Holamundo/Program.cs
+++ HolaMundo/Holamundo/Holamundo/Program.cs
@@ -16,10 +16,43 @@ namespace Holamundo
             string nombre = Console.ReadLine();
             Console.WriteLine("Ingrese Edad: ");
             string edadtx = Console.ReadLine().Trim();
-            //TrimStart(); Quita espacios al inicio
-            //TrimEnd(); Quita espacios al final
-            Console.WriteLine("Su nombre es {0}", nombre);    
+            int edad = -1;
+            bool esValido = Int32.TryParse(edadtx, out edad);
+
+            if (edad == -1) //if (!esValido) es decir If (esvalido == true) 
+            {
+                Console.WriteLine("Ingrese bien la edad");
+            }
+            else
+            {
+                Console.WriteLine("Su nombre es {0} y su edad es {1}", nombre, edad);
+            }
+              
             Console.ReadKey();
         }
     }
 }
+
+
+
+
+//TrimStart(); Quita espacios al inicio
+//TrimEnd(); Quita espacios al final
+
+// EXPLICACION
+/*
+string num = "12";
+int num2;
+
+bool esValido = int.TryParse(num, out num2);
+
+try
+{
+    string num = "12";
+    int num2 = Convert.ToInt32(num);
+    return true;
+}catch (Exception ex)
+{
+    return false;
+}
+*/
