0d60f3e7a5a72e456e0609b3308b00a9bf04d20c
orionpgaete <pedro.gaete@pgcomputer.cl>
2022-04-18 16:20:05 -0400

    ClienteHolaMundoS

---
+++ HolaMundoSocket/ClienteHolaMundoS/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClienteHolaMundoS.Comunicacion;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteHolaMundoS
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = ConfigurationManager.AppSettings["servidor"];
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectado a Servidor {0} en puerto {1}", servidor, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+            if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Conectado...");
+                string mensaje = clienteSocket.Leer();
+                Console.WriteLine("M: {0}", mensaje);
+                string nombre = Console.ReadLine().Trim();
+                clienteSocket.Escribir(nombre);
+                mensaje = clienteSocket.Leer();
+                Console.WriteLine("M: {0}", mensaje);
+                clienteSocket.Desconectar();
+            }
+            else
+            {
+                Console.WriteLine("Error de Comunicacion");
+            }
+            Console.ReadKey();
+        }
+    }
+}
